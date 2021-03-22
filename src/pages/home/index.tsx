185d3c77dd6010e6c4d68ab43569f8ceefd59360
Kei Mori <devninjamori@gmail.com>
2021-03-22 09:21:57 -0700

    Add stylelint & update structure

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from 'src/assets/images/logo.svg';
+import styles from './style.module.scss';
+
+const Home = (): React.ReactElement => {
+  return (
+    <div className={styles.app}>
+      <header className={styles.appHeader}>
+        <img src={logo} className={styles.appLogo} alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className={styles.appLink}
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/pages/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import Home from './home';
+
+const Pages = (): React.ReactElement => {
+  return <Home />;
+};
+
+export default Pages;
