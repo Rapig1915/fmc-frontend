0b65d9557b0f8d7d0cce39bf6f31474fce465734
Rapig1915 <rapig1915@gmail.com>
2021-05-31 15:40:16 -0500

    CustomAccordion component

---
+++ src/components/organisms/CustomAccordion/CustomAccordion.tsx
@@ -0,0 +1,59 @@
+import {
+  Accordion,
+  AccordionDetails,
+  AccordionSummary,
+  withStyles,
+} from '@material-ui/core';
+
+export const MyAccordion = withStyles((theme) => ({
+  root: {
+    marginTop: theme.spacing(1),
+    marginBottom: theme.spacing(1),
+    border: '1px solid #DBE8FD',
+    minWidth: 350,
+    borderRadius: 6,
+    boxShadow: 'none',
+    '&:not(:last-child)': {},
+    '&:before': {
+      display: 'none',
+    },
+    '&$expanded': {
+      marginTop: theme.spacing(1),
+      marginBottom: theme.spacing(1),
+    },
+
+    [theme.breakpoints.down('xs')]: {
+      minWidth: 200,
+    },
+  },
+  expanded: {},
+}))(Accordion);
+
+export const MyAccordionSummary = withStyles({
+  root: {
+    backgroundColor: '#EBF1FA',
+    borderBottom: '1px solid #EBF1FA',
+    borderRadius: 6,
+    marginBottom: -1,
+    minHeight: 56,
+    '&$expanded': {
+      minHeight: 56,
+    },
+  },
+  content: {
+    '&$expanded': {
+      margin: '12px 0',
+    },
+
+    fontSize: 19,
+    lineHeight: '28px',
+    fontWeight: 'bold',
+  },
+  expanded: {},
+})(AccordionSummary);
+
+export const MyAccordionDetails = withStyles((theme) => ({
+  root: {
+    padding: theme.spacing(2),
+  },
+}))(AccordionDetails);
